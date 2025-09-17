900bc65cc413b36047b2a677bb8d98e335c84861
Sergio Ramos <sergioramos_92@hotmail.com>
2025-09-17 19:46:50 -0300

    se agrega atributo loading con valor 'lazy' para mejorar la experiencia de usuario en la carga de imágenes

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -14,7 +14,7 @@ function MealCard({ meal, handleImageLoad }: MealCardProps) {
   }
   return (
     <div className="meal-card" key={meal.idMeal} onClick={() => handleMealClick(meal.idMeal)}>
-      {meal.strMealThumb && <img onLoad={handleImageLoad} src={meal.strMealThumb} alt={meal.strMeal} />}
+      {meal.strMealThumb && <img loading="lazy" onLoad={handleImageLoad} src={meal.strMealThumb} alt={meal.strMeal} />}
       <h3>{meal.strMeal}</h3>
     </div>
   );
